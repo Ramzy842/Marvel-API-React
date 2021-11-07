46444837ec81667abc6121381f0df2019494690f
Ramzy842 <ramzychahbani@gmail.com>
2021-11-07 08:15:26 +0100

    Marvel-API-React

--- src/Data/Categories.js
+++ src/Data/Categories.js
@@ -7,18 +7,7 @@ const categories = [
     path: "/mcu-avengers/characters",
     color: "#07b1ff"
   },
-  {
-    title: "Eternals",
-    image: avengersImage,
-    path: "/mcu-avengers/characters",
-    color: "red"
-  },
-  {
-    title: "MCU Avengers",
-    image: avengersImage,
-    path: "/mcu-avengers/characters",
-    color: "green"
-  },
+  
   
   
   
